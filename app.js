81ae913f38597a9a20fce3613abf59102e3a6eee
exploitenomah <iksploiting@gmail.com>
2023-02-20 12:38:23 +0100

    set up dotenvg

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 
 const app = express()
-
-// view engine setup
+require('dotenv').config({
+  path: path.resolve(__dirname, '.env'),
+})
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 app.use(cors())
@@ -25,18 +26,18 @@ app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404))
+  next(createError(404))
 })
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-    // render the error page
-    res.status(err.status || 500)
-    res.render('error')
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
 })
 
 module.exports = app
