3f4c15f86dc524174bc1e2cea0cce2edbf337567
exploitenomah <iksploiting@gmail.com>
2023-02-20 09:31:51 +0100

    create namespaces

---
+++ namespaces.js
@@ -0,0 +1,23 @@
+const namespacesSrc = {
+  users: '/users',
+  chatrooms: '/chatrooms',
+  messages: '/messages',
+}
+module.exports.namespacesEventsAndHandlers = {
+  [namespacesSrc.users]: {
+    ping: (io, socket, data) => {
+      socket.emit('pong', '/users')
+    },
+  },
+  [namespacesSrc.chatrooms]: {
+    ping: (io, socket, data) => {
+      socket.emit('pong', '/chatrooms')
+    },
+  },
+  [namespacesSrc.messages]: {
+    ping: (io, socket, data) => {
+      socket.emit('pong', '/messages')
+    },
+  },
+}
+module.exports.namespaces = Object.values(namespacesSrc)
