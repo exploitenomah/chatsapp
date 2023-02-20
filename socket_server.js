986ade02a148c38476720812c5016739c638602b
exploitenomah <iksploiting@gmail.com>
2023-02-20 09:31:29 +0100

    create handler for namespaces

--- socket_server.js
+++ socket_server.js
@@ -1,6 +1,49 @@
-const { Server } = require("socket.io");
+const { Server } = require('socket.io')
+const { namespacesEventsAndHandlers } = require('./namespaces')
 
 module.exports = function startSocketServer(server) {
-	const io = new Server(server);
-	return io
+  const io = new Server(server, {
+    cors: {
+      origin: 'http://localhost:3001',
+      methods: ['GET', 'POST'],
+    },
+  })
+  return io
+}
+
+const socketListening = (io, socket, events = {}) => {
+  const eventNames = Object.keys(events)
+  eventNames.map((eventName) =>
+    socket.on(eventName, (req) => {
+      events[eventName](io, socket, req)
+    }),
+  )
+}
+
+const onConnect = (socket, connections = new Set()) => {
+  connections.add(socket)
+  return connections
+}
+
+module.exports.ioListening = (io) => {
+  let connections = new Set()
+  return io.on('connection', (socket) => {
+    connections = onConnect(socket, connections)
+    socket.on('disconnect', () => {
+      connections.delete(socket)
+    })
+    socketListening(io, socket)
+  })
+}
+
+module.exports.namespaceListening = (io, namespace) => {
+  let connections = new Set()
+  io.of(namespace).on('connection', (socket) => {
+    connections = onConnect(socket, connections)
+    socket.on('disconnect', () => {
+      connections.delete(socket)
+    })
+    const namespaceEvents = namespacesEventsAndHandlers[namespace]
+    socketListening(io, socket, namespaceEvents)
+  })
 }
