1bade5dbb6639f5b4a8f622ca1089c50a84cdb9a
exploitenomah <iksploiting@gmail.com>
2023-02-20 09:30:53 +0100

    setup namespaces

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = require('./app')
 const debug = require('debug')('backend:server')
 const http = require('http')
 const startSocketServer = require('./socket_server')
+const { ioListening, namespaceListening } = require('./socket_server')
+const { namespaces } = require('./namespaces')
 
 const port = normalizePort(process.env.PORT || '3000')
 app.set('port', port)
@@ -14,42 +16,44 @@ server.on('error', onError)
 server.on('listening', onListening)
 
 const io = startSocketServer(server)
-console.log(io)
+ioListening(io)
+namespaces.map((namespace) => namespaceListening(io, namespace))
+
 function normalizePort(val) {
-    const port = parseInt(val, 10)
+  const port = parseInt(val, 10)
 
-    if (isNaN(port)) {
-        return val
-    }
+  if (isNaN(port)) {
+    return val
+  }
 
-    if (port >= 0) {
-        return port
-    }
+  if (port >= 0) {
+    return port
+  }
 
-    return false
+  return false
 }
 
 function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error
-    }
+  if (error.syscall !== 'listen') {
+    throw error
+  }
 
-    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
 
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges')
-            process.exit(1)
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use')
-            process.exit(1)
-        default:
-            throw error
-    }
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use')
+      process.exit(1)
+    default:
+      throw error
+  }
 }
 
 function onListening() {
-    const addr = server.address()
-    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
-    debug('Listening on ' + bind)
+  const addr = server.address()
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
+  debug('Listening on ' + bind)
 }
