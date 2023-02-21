4bac00030a7f7cb87df08e03f7f0217887043f28
exploitenomah <iksploiting@gmail.com>
2023-02-21 14:21:24 +0100

    complete authentication and authorization

--- socket/middleware/auth.js
+++ socket/middleware/auth.js
@@ -1,8 +1,12 @@
+const { getUser } = require('../../controllers/user')
+const { verifyJWT } = require('../../utils/security')
+
 module.exports.authenticate = async (socket, next) => {
   const token = socket.handshake.auth?.token
-  if (!token) {
-    console.log('unauthorized')
-    return next(new Error('Unauthorized!!!'))
-  }
+  if (!token) return next(new Error('Unauthorized!!!'))
+  const tokenPayload = verifyJWT(token)
+  const user = getUser(tokenPayload)
+  if (!user) return next(new Error('Unauthorized!!!'))
+  socket.user = user
   next()
 }
