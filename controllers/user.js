6e364bf131cd18115cf7143943b152f4bddc25d4
exploitenomah <iksploiting@gmail.com>
2023-02-21 10:16:08 +0100

    create login controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,24 @@
 const { User } = require('../models/user')
+const DocumentController = require('../utils/document')
+const { signJWT } = require('../utils/security')
+
+const UserController = new DocumentController(User)
+
+module.exports.loginUser = async (data) => {
+  const filter = { ...data }
+  delete filter.password
+  const user = await module.exports.getUser(filter)
+  const passwordVerified = await user.verifyPassword(data.password)
+  if (passwordVerified) {
+    user.password = undefined
+    return { user, token: signJWT({ key: '_id', val: user._id }) }
+  }
+  return passwordVerified
+}
 
 module.exports.createUser = async (data) => {
-  console.log(data)
-  let newUser = await User.create({ ...data })
-  newUser = await newUser.save()
-  return newUser
+  return await UserController.createDoc({ ...data })
+}
+module.exports.getUser = async (filter, select) => {
+  return await UserController.getDoc(filter, select)
 }
