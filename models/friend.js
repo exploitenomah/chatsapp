5005208b0e74e69764c7041d5ad47a3d901e3ae7
exploitenomah <iksploiting@gmail.com>
2023-02-23 13:03:14 +0100

    create friend schema

---
+++ models/friend.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const friendSchema = new mongoose.Schema(
+  {
+    requester: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, 'Please specify the requester'],
+      ref: 'User',
+      validate: function (val) {
+        return !this.recipient.equals(val)
+      },
+    },
+    is_valid: {
+      type: Boolean,
+      default: false,
+    },
+    recipient: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, 'Please specify the recipient'],
+      ref: 'User',
+      validate: function (val) {
+        return !this.requester.equals(val)
+      },
+    },
+  },
+  {
+    timestamps: true,
+  },
+)
+
+const Friend = new mongoose.model('Friend', friendSchema)
+module.exports = Friend
