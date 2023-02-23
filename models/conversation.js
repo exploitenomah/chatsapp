e1033e9fdb6eda7fc8ab62877124473a0f366981
exploitenomah <iksploiting@gmail.com>
2023-02-23 17:25:05 +0100

    create conversation model

---
+++ models/conversation.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose')
+const { User } = require('../models/user')
+
+const conversationSchema = new mongoose.Schema(
+  {
+    creator: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    participants: {
+      type: [
+        {
+          type: mongoose.Schema.ObjectId,
+          ref: 'User',
+        },
+      ],
+      validate: [
+        async function (val) {
+          const allAreValidUsers = [
+            ...(await Promise.all(
+              val.map(
+                async (el) => (await User.countDocuments({ _id: el })) > 0,
+              ),
+            )),
+          ].every((val) => val === true)
+
+          const conversationWithParticipantsExists =
+            await this.constructor.find({
+              participants: val,
+            })
+
+          const setFromVal = new Set(val.map((el) => JSON.stringify(el)))
+          return (
+            setFromVal.size === val.length &&
+            setFromVal.size > 1 &&
+            !conversationWithParticipantsExists &&
+            allAreValidUsers
+          )
+        },
+        'participants must be unique and greater than 1',
+      ],
+    },
+    latest_msg: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Message',
+    },
+  },
+  {
+    timestamps: true,
+  },
+)
+
+conversationSchema.pre(/^find/, function (next) {
+  if (this.options._recursed) {
+    return next()
+  }
+  this.populate({
+    path: 'participants',
+    options: { _recursed: true },
+    select: 'nick_name profile_pic',
+  }).populate({ path: 'latest_msg', options: { _recursed: true } })
+  next()
+})
+
+module.exports = mongoose.model('Conversation', conversationSchema)
