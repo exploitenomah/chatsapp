3099351bb062d93acabf2435ed7ee9a4d5edbd0d
exploitenomah <iksploiting@gmail.com>
2023-02-23 17:25:49 +0100

    create conversation namespace handlers

---
+++ socket/namespaces/conversations.js
@@ -0,0 +1,46 @@
+const {
+  createConversation,
+  getConversation,
+  updateConversation,
+  getMany,
+} = require('../../controllers/conversation')
+const { socketTryCatcher } = require('../../utils/tryCatcher')
+
+const events = {
+  new: 'new',
+  update: 'update',
+  getOne: 'getOne',
+  getMany: 'getMany',
+}
+
+module.exports.conversationEventHandlers = {
+  [events.new]: socketTryCatcher(async (_io, socket, data) => {
+    const newConversation = await createConversation({
+      ...data,
+      creator: socket.user._id,
+    })
+    socket.emit(events.new, newConversation)
+  }),
+  [events.getOne]: socketTryCatcher(async (_io, socket, data) => {
+    const conversation = await getConversation({
+      ...data,
+      participants: { $in: socket.user._id },
+    })
+    socket.emit(events.getOne, conversation)
+  }),
+  [events.update]: socketTryCatcher(async (_io, socket, data) => {
+    const { query, update } = data
+    const updConversation = await updateConversation(
+      { ...(query || {}), participants: { $in: socket.user._id } },
+      update,
+    )
+    socket.emit(events.update, updConversation)
+  }),
+  [events.getMany]: socketTryCatcher(async (_io, socket, data) => {
+    const conversations = await getMany({
+      ...data,
+      participants: { $in: socket.user._id },
+    })
+    socket.emit(events.getMany, conversations)
+  }),
+}
