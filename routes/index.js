654ec2138e44cfa69ad3c80b368f4670fc3acb5e
exploitenomah <iksploiting@gmail.com>
2023-02-25 13:26:50 +0100

    modify index route to return status of 200 on all requests if alive

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+router.all('/', function (req, res, next) {
+  res.status(200).json({ alive: true })
+})
+module.exports = router
