38ce77064644110bf37a4eceaa4ffce1185603dc
exploitenomah <iksploiting@gmail.com>
2023-03-03 15:06:51 +0100

    create namespace for friends

--- socket/namespaces/index.js
+++ socket/namespaces/index.js
@@ -1,16 +1,19 @@
 const { userEventHandlers } = require('./users')
 const { conversationEventHandlers } = require('./conversations')
 const { messageEventHandlers } = require('./messages')
+const { friendsEventHandlers } = require('./friends')
 
 const namespacesSrc = {
   users: '/users',
   conversations: '/conversations',
   messages: '/messages',
+  friends: '/friends',
 }
 
 module.exports.namespacesEventsHandlers = {
   [namespacesSrc.users]: userEventHandlers,
   [namespacesSrc.conversations]: conversationEventHandlers,
   [namespacesSrc.messages]: messageEventHandlers,
+  [namespacesSrc.friends]: friendsEventHandlers,
 }
 module.exports.namespaces = Object.values(namespacesSrc)
