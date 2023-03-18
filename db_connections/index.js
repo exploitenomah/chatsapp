8384e8a261e5bc7b29b65b39b9b8eb9d5a0a10c6
exploitenomah <iksploiting@gmail.com>
2023-03-18 09:18:29 +0100

    add local connection

--- db_connections/index.js
+++ db_connections/index.js
@@ -1,8 +1,14 @@
 const devConn = require('./dev')
 const prodConn = require('./prod')
 const testConn = require('./test')
+const localConn = require('./local')
 
-const connections = { test: testConn, dev: devConn, prod: prodConn }
+const connections = {
+  test: testConn,
+  dev: devConn,
+  prod: prodConn,
+  local: localConn,
+}
 
 module.exports = function (onConnectCb) {
   const nodeEnv = process.env.NODE_ENV
