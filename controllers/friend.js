a4704ad9a237159dc8ba80128932ed89bf54daeb
exploitenomah <iksploiting@gmail.com>
2023-03-18 12:58:23 +0100

    sterilize friends query

--- controllers/friend.js
+++ controllers/friend.js
@@ -1,6 +1,13 @@
 const DocumentController = require('../utils/document')
 const Friend = require('../models/friend')
 const { getMany: getManyUsers } = require('./user')
+const { sterilizeQuery } = require('../utils')
+
+const allowedQueryPaths = ['requester', 'recipient', 'isValid', '_id', 'id']
+
+const sterilizeFriendsQuery = (query) => {
+  return sterilizeQuery(allowedQueryPaths, query)
+}
 
 const FriendController = new DocumentController(Friend)
 
@@ -8,15 +15,19 @@ module.exports.createFriend = async (data) => {
   return await FriendController.createDoc({ ...data })
 }
 module.exports.getFriend = async (filter, select) => {
-  return await FriendController.getDoc(filter, select)
+  return await FriendController.getDoc(sterilizeFriendsQuery(filter), select)
 }
 module.exports.updateFriend = async (filter, update) => {
-  return await FriendController.updateDoc(filter, update, {
-    returnOriginal: false,
-  })
+  return await FriendController.updateDoc(
+    sterilizeFriendsQuery(filter),
+    update,
+    {
+      returnOriginal: false,
+    },
+  )
 }
 module.exports.deleteFriend = async (filter) => {
-  return await FriendController.deleteDoc(filter)
+  return await FriendController.deleteDoc(sterilizeFriendsQuery(filter))
 }
 module.exports.getMany = async (searchQuery) => {
   return await FriendController.getMany(searchQuery)
