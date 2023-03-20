cb9901602de0a02a9a7f5c4d27f344ea9ca92c83
exploitenomah <iksploiting@gmail.com>
2023-03-20 10:16:48 +0100

    create ip utils

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,6 @@
+const fetch = require('node-fetch')
+const { isIP } = require('net')
+
 module.exports.sterilizeObject = (allowedPaths = [], query = {}) => {
   const sterilizedQueryObj = {}
   allowedPaths.forEach((path) => {
@@ -6,4 +9,22 @@ module.exports.sterilizeObject = (allowedPaths = [], query = {}) => {
     }
   })
   return sterilizedQueryObj
+}
+module.exports.getIpFromSocket = (socket = {}) => {
+  const ipAddress = socket.request?.connection?.remoteAddress
+  if (typeof ipAddress !== 'string') return false
+  return ipAddress
+}
+
+module.exports.getGeoLocationInfoFromIpAddress = async (ipAddress) => {
+  try {
+    const ipVersion = isIP(ipAddress)
+    if (ipVersion !== 4 || ipVersion !== 6) {
+      return { error: true, reason: 'invalid ip address' }
+    }
+    const response = await fetch(`https://ipapi.co/${ipAddress}/json/`)
+    return response.json()
+  } catch (err) {
+    console.log(err, 'getGeoLocationInfoFromIpAddress')
+  }
 }
