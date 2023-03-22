62002e04a9e087aec54d23e49a8707ea5299c23a
exploitenomah <iksploiting@gmail.com>
2023-03-22 05:09:23 +0100

    update suggestions aggregation to include other fields of user

--- aggregations/user.js
+++ aggregations/user.js
@@ -2,12 +2,12 @@ const {
   Types: { ObjectId },
 } = require('mongoose')
 
-module.exports.friendsSuggestionsAggregator = (userId, limit, page) => {
+module.exports.friendsSuggestionsAggregator = (user, limit, page) => {
   return [
     {
       $match: {
         _id: {
-          $ne: new ObjectId(userId),
+          $ne: new ObjectId(user._id),
         },
       },
     },
@@ -31,15 +31,27 @@ module.exports.friendsSuggestionsAggregator = (userId, limit, page) => {
     {
       $project: {
         friends: { $setUnion: ['$requester', '$recipient'] },
+        friendsCount: { $size: { $setUnion: ['$requester', '$recipient'] } },
         firstName: 1,
         lastName: 1,
         nickName: 1,
         email: 1,
-        friendsCount: { $size: { $setUnion: ['$requester', '$recipient'] } },
+        region: 1,
+        regionCode: 1,
+        city: 1,
+        countryName: 1,
+        utcOffset: 1,
+        countryCode: 1,
       },
     },
-    { $match: { friendsCount: { $lte: 0 } } },
-    // { $sort: { createdAt: 1 } },
+    {
+      $match: {
+        friendsCount: { $lte: 0 },
+        countryCode: { $gte: user.countryCode },
+        region: { $gte: user.region },
+      },
+    },
+    { $match: {} },
     { $limit: limit },
     { $skip: (page - 1) * limit },
   ]
