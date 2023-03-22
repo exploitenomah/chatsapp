664dcd032050fccef35493db283ce30599c5f2e0
exploitenomah <iksploiting@gmail.com>
2023-03-22 05:11:01 +0100

    populate friends data, check if friend exists before create

--- controllers/friend.js
+++ controllers/friend.js
@@ -1,12 +1,13 @@
 const DocumentController = require('../utils/document')
-const Friend = require('../models/friend')
+const { Friend, userPopulateOptions } = require('../models/friend')
 const { friendsSuggestionsAggregator } = require('../aggregations/user')
 const { sterilizeObject } = require('../utils')
 const { universalQueryPaths } = require('../utils/constants')
 
 const { User } = require('../models/user')
+const { getUser } = require('./user')
 
-const allowedUpdatePaths = ['requester', 'recipient', 'isValid']
+const allowedUpdatePaths = ['requester', 'recipient', 'isValid', 'seen']
 
 const allowedQueryPaths = [...universalQueryPaths, ...allowedUpdatePaths]
 
@@ -17,7 +18,25 @@ const sterilizeFriendsQuery = (query) => {
 const FriendController = new DocumentController(Friend)
 
 module.exports.createFriend = async (data) => {
-  return await FriendController.createDoc({ ...data })
+  const existingFriendshipIdData = await FriendController.checkIfExists({
+    $or: [
+      {
+        requester: data.requester,
+        recipient: data.recipient,
+      },
+      {
+        requester: data.recipient,
+        recipient: data.requester,
+      },
+    ],
+  })
+  if (existingFriendshipIdData) {
+    const existingFriendship = await module.exports.getFriend({
+      _id: existingFriendshipIdData._id.toString(),
+    })
+    console.log(existingFriendship, 'fooxlosz')
+    return await existingFriendship
+  } else return await FriendController.createDoc({ ...data })
 }
 module.exports.getFriend = async (filter, select) => {
   return await FriendController.getDoc(sterilizeFriendsQuery(filter), select)
@@ -35,11 +54,16 @@ module.exports.deleteFriend = async (filter) => {
   return await FriendController.deleteDoc(sterilizeFriendsQuery(filter))
 }
 module.exports.getMany = async (searchQuery) => {
-  return await FriendController.getMany(searchQuery)
+  const friends = await FriendController.getMany(
+    searchQuery,
+    userPopulateOptions,
+  )
+  return await friends
 }
 
 module.exports.getFriendsSuggestions = async ({ userId, page, limit }) => {
-  const aggregator = friendsSuggestionsAggregator(userId, limit, page)
+  const user = await getUser({ _id: userId })
+  const aggregator = friendsSuggestionsAggregator(user, limit, page)
   const suggestions = await User.aggregate(aggregator)
   return suggestions
 }
