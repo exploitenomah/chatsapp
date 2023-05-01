3de46c05d6390ca0fe4edbf621d453e68bb56068
exploitenomah <iksploiting@gmail.com>
2023-05-01 07:29:20 +0100

    fix multer upload

--- routes/images.js
+++ routes/images.js
@@ -2,13 +2,21 @@ const express = require('express')
 const { getParser } = require('../utils/imageParser')
 const router = express.Router()
 
-router.post('/profile-images', (req, res, next) => {
-  getParser('profile-images', 'profile-preset').single('image')(req, res, next)
-  if (req.file) {
-    res.status(201).json({
-      file: req.file,
-    })
-  }
-})
+router.post(
+  '/profile-images',
+  getParser('profile-images', 'profile-preset').single('image'),
+  (req, res) => {
+    console.log(req.file)
+    if (req.file) {
+      res.status(201).json({
+        file: req.file,
+      })
+    } else {
+      res.status(500).json({
+        message: 'something went wrong',
+      })
+    }
+  },
+)
 
 module.exports = router
