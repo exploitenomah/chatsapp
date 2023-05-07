96c18edf8d13051bd532045306e09c7eb198ef1a
exploitenomah <iksploiting@gmail.com>
2023-05-07 23:31:02 +0100

    create blockings controller

---
+++ controllers/blockings.js
@@ -0,0 +1,41 @@
+const { Blocking } = require('../models/blocking')
+const DocumentController = require('../utils/document')
+const { sterilizeObject } = require('../utils')
+const { universalQueryPaths } = require('../utils/constants')
+
+const BlockingsController = new DocumentController(Blocking)
+
+const allowedUpdatePaths = ['blocker', 'blockee']
+
+const allowedQueryPaths = [...universalQueryPaths, ...allowedUpdatePaths]
+
+const sterilizeBlockingsQuery = (query) => {
+  return sterilizeObject(allowedQueryPaths, query)
+}
+
+module.exports.createBlocking = async (data) => {
+  const existingBlocking = await BlockingsController.checkIfExists({
+    $or: [
+      {
+        blocker: data.blocker,
+        blockee: data.blockee,
+      },
+      {
+        blocker: data.blockee,
+        blockee: data.blocker,
+      },
+    ],
+  })
+  if (existingBlocking) return existingBlocking
+  const blocking = await BlockingsController.createDoc({ ...data })
+  return blocking
+}
+module.exports.getBlocking = async (filter, select) => {
+  return await BlockingsController.getDoc(
+    sterilizeBlockingsQuery(filter),
+    select,
+  )
+}
+module.exports.deleteBlocking = async (filter) => {
+  return await BlockingsController.deleteDoc(sterilizeBlockingsQuery(filter))
+}
