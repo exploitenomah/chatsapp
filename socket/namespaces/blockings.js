a29a6dad58f3bcac482b5338b82e63e2bccb813e
exploitenomah <iksploiting@gmail.com>
2023-05-07 23:31:48 +0100

    create blocking namespace

---
+++ socket/namespaces/blockings.js
@@ -0,0 +1,41 @@
+const {
+  createBlocking,
+  getBlocking,
+  deleteBlocking,
+} = require('../../controllers/blockings')
+const { socketTryCatcher } = require('../../utils/tryCatcher')
+
+const events = {
+  block: 'block',
+  unblock: 'unblock',
+  getOne: 'getOne',
+}
+
+module.exports.blockingsEventHandlers = {
+  [events.block]: socketTryCatcher(async (_io, socket, data = {}) => {
+    const blocking = await createBlocking({
+      blockee: data.blockee,
+      blocker: socket.user._id,
+    })
+    if (
+      blocking &&
+      blocking.blocker.toString() === socket.user._id.toString()
+    ) {
+      socket.emit(events.block, blocking)
+      socket.to(blocking.blockee._id.toString()).emit(events.block, blocking)
+    } else {
+      socket.emit('error', 'something went wrong')
+    }
+  }),
+  [events.getOne]: socketTryCatcher(async (_io, socket, data = {}) => {
+    const blocking = await getBlocking({ ...data })
+    socket.emit(events.getOne, blocking)
+  }),
+  [events.unblock]: socketTryCatcher(async (_io, socket, data) => {
+    await deleteBlocking({
+      _id: data.blockingId,
+      blocker: socket.user._id,
+    })
+    socket.emit(events.unblock, { unblocked: true })
+  }),
+}
